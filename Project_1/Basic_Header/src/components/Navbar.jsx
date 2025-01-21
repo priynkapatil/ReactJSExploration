33c1f4529390431c7efb20fb395b668c280f5415
Priyanka Patil <priyankapatilp22@gmail.com>
2025-01-21 08:38:46 +0530

    added hamburger menu

--- Project_1/Basic_Header/src/components/Navbar.jsx
+++ Project_1/Basic_Header/src/components/Navbar.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/brand_logo.svg';
 import '../Css/Navbar.css';
+import NavbarLinks from './NavbarLinks';
+import { MdOutlineMenu } from 'react-icons/md';
+
+import { MdClose } from 'react-icons/md';
+
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const Hamburger = (
+    <MdOutlineMenu
+      className='HamburgerMenu'
+      size='30px'
+      color='black'
+      onClick={() => setClick(!click)}
+    />
+  );
+
+  const Close = (
+    <MdClose
+      className='HamburgerMenu'
+      size='30px'
+      color='black'
+      onClick={() => setClick(!click)}
+    />
+  );
+
   return (
     <div className='nav-align'>
       <div>
         <img src={logo} alt='logo' />
       </div>
-      <div>
-        <ul className='menu'>
-          <li>MENU</li>
-          <li>Location</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+      <div className='DesktopNavigation'>
+        <NavbarLinks />
       </div>
       <div>
         <button className='btn-nav'>Login</button>
+      </div>
+      <div className='MobileNavigation' >
+        {click ? Hamburger :Close }
+        <div>
+        {!click && <NavbarLinks />}
+        </div>
+        
       </div>
     </div>
   );
---
+++ Project_1/Basic_Header/src/components/NavbarLinks.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Css/Navbar.css';
+
+const NavbarLinks = () => {
+  return (
+    <ul className='menu'>
+          <li>MENU</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+  )
+}
+
+export default NavbarLinks
