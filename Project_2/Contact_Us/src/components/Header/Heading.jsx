961de43fa1cac66b51054c3e3b0127c8a36cd8d1
Priyanka Patil <priyankapatilp22@gmail.com>
2025-01-24 00:00:37 +0530

    header changes

--- Project_2/Contact_Us/src/components/Header/Heading.jsx
+++ Project_2/Contact_Us/src/components/Header/Heading.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import BannerForm from './BannerForm';
 const Heading = () => {
   return (
     <div>
-        <p className='heading'>
-        CONTACT US
-        </p>
-        <p className='subHeading'>
-        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. 
-        </p>
+      <p className='heading'>CONTACT US</p>
+      <p className='subHeading'>
+        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+        WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
+        REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
+        EMAIL, OR SOCIAL MEDIA.
+      </p>
+      <div>
+        <BannerForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Heading
+export default Heading;
